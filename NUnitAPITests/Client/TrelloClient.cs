834c12894d264001c5254505288ba819d5a033fd
agonzales411 <agv417@gmail.com>
2020-09-16 23:09:45 -0400

    add Trello tests

---
+++ NUnitAPITests/Client/TrelloClient.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnitAPITests.Config;
+using RestSharp;
+
+namespace NUnitAPITests.Client
+{
+    public class TrelloClient : IClient
+    {
+        public static TrelloClient instance;
+
+        private RestClient client;
+
+        private TrelloClient()
+        {
+            client = new RestClient(EnvironmentConfig.GetInstance().GetBaseUrl(ApisEnum.Trello));  
+        }
+
+        public static TrelloClient GetInstance()
+        {
+            return instance == null ? new TrelloClient() : instance;
+        }
+
+        public RestClient GetClient()
+        {
+            return client;
+        }
+    }
+}
---
+++ NUnitAPITests/Tests/Trello/PostBoardTests.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using NUnit.Framework;
+using NUnitAPITests.Client;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NUnitAPITests.Tests.Trello
+{
+    public class PostBoardTests
+    {
+        private List<string> boardsIds;
+
+        [SetUp]
+        public void Setup()
+        {
+            boardsIds = new List<string>();
+        }
+
+        [Test]
+        public void PostBoardTest()
+        {
+            // Build request
+            var request = new TrelloRequest("boards");
+            var expectedName = "Automation Trello Request";
+            var expectedDefaultLabels = false;
+            var expectedDesc = "Board Description";
+            var expectedPrefsBackground = "lime";
+            var expectedPrefsCardAging = "pirate";
+
+            //JsonBody
+            var jsonRequest = new
+            {
+                name = expectedName,
+                defaultLabels = expectedDefaultLabels,
+                desc = expectedDesc,
+                prefs_background = expectedPrefsBackground,
+                prefs_cardAging = expectedPrefsCardAging
+            };
+
+            //Add Body requests
+            request.GetRequest().AddJsonBody(jsonRequest);
+            var response = RequestManager.Post(TrelloClient.GetInstance(), request);
+
+            Assert.AreEqual(200, (int)response.StatusCode);
+
+            var jsonObject = JObject.Parse(response.Content);
+
+            //Save Project ID
+            boardsIds.Add(jsonObject.SelectToken("id").ToString());
+
+            //Validate actualOutputs Fields            
+            Assert.AreEqual(expectedName, jsonObject.SelectToken("name").ToString());
+            Assert.AreEqual(expectedDesc, jsonObject.SelectToken("desc").ToString());
+            Assert.AreEqual(expectedPrefsBackground, jsonObject.SelectToken("prefs.background").ToString());
+            Assert.AreEqual(expectedPrefsCardAging, jsonObject.SelectToken("prefs.cardAging").ToString());
+
+            //Schema Validation
+            var jsonSchemaString = File.ReadAllText("Schemas/Trello/PostBoardSchema.json");
+            var jsonSchema = JSchema.Parse(jsonSchemaString);
+
+            IList<string> schemaErrors = new List<string>();
+            Assert.IsTrue(jsonObject.IsValid(jsonSchema, out schemaErrors));
+        }
+
+        [TearDown]
+        public void DeleteBoards()
+        {
+            foreach (var id in boardsIds)
+            {
+                var request = new TrelloRequest("boards/" + id);
+                RequestManager.Delete(TrelloClient.GetInstance(), request);
+            }
+        }
+    }
+}
