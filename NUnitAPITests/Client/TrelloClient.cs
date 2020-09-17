98d495509bd615579adbd3938a9f880f8be9d073
dxni <33109901+dxni@users.noreply.github.com>
2020-09-16 21:13:08 -0400

    [API-Automation-CSharp] Task 03 - Nunit API Tests

---
+++ NUnitAPITests/Client/TrelloClient.cs
@@ -0,0 +1,28 @@
+﻿using NUnitAPITests.Config;
+using RestSharp;
+
+namespace NUnitAPITests.Client
+{
+    public sealed class TrelloClient : IClient
+    {
+        private static TrelloClient instance;
+        private RestClient client;
+
+        private TrelloClient()
+        {
+            client = new RestClient(EnvironmentConfig.GetInstance().GetBaseUrl(ApisEnum.Trello));
+        }
+
+        public static TrelloClient GetInstance()
+        {
+            if (instance == null) instance = new TrelloClient();
+
+            return instance;
+        }
+
+        public RestClient GetClient()
+        {
+            return client;
+        }
+    }
+}
